0ac6fea9c6eb5eb0ce13a1969362565292cb0fc0
Abraham Quaye <abrahamquaye@Mac-Studio.local>
2026-01-23 10:37:41 -0600

    Added script to concatenate same strand fastqs

---
+++ scripts/shell/concatenate_reads.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+raw_dir="/Users/abrahamquaye/myocd_rnaseq/raw_files"
+
+fastq_dirs=( $raw_dir/siO_* )
+
+mkdir -p $raw_dir/merged_fastqs
+
+ echo "Concatenating same-strand reads for all samples ..."
+for d in ${fastq_dirs[@]}; do
+    dir_name=$(basename $d)
+    echo $dir_name
+    cat $d/${dir_name}_L001_R*_001.fastq.gz > $raw_dir/merged_fastqs/${dir_name}_R1_merged.fastq.gz
+    cat $d/${dir_name}_L002_R*_001.fastq.gz > $raw_dir/merged_fastqs/${dir_name}_R2_merged.fastq.gz
+done
